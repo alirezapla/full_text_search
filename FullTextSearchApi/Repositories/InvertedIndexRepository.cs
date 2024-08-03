8d05ed70d3556564b245fee56761ae584c3ca57b
alireza hajli <hajlialireza@gmail.com>
2024-08-03 14:45:02 +0330

    fix: use FindAsync in get method

--- FullTextSearchApi/Repositories/InvertedIndexRepository.cs
+++ FullTextSearchApi/Repositories/InvertedIndexRepository.cs
@@ -9,18 +9,14 @@ namespace FullTextSearchApi.Repositories;
 public class InvertedIndexRepository(SearchDbContext context)
     : BaseRepository<InvertedIndex>(context), IInvertedIndexRepository
 {
-    public async Task<List<String>> Get(String query)
+    public async Task<List<string>> Get(string query)
     {
-        // var data = await FindAsync(index => index.Word == query.ToUpper());
-        var data = await Context.InvertedIndex.Where(x => x.Word == query.ToUpper())
-            .Select(x => x.DocumentId)
-            .ToListAsync();
-        // return data.Select(x=>x.DocumentId).ToList();
-        return data;
+        var data = await FindAsync(index => index.Word == query.ToUpper());
+        return data.Select(x=>x.DocumentId).ToList();
     }
 
-    public async Task<List<String>> ConditionalGet(
-        HashSet<String> allWords, HashSet<String> anyWords, HashSet<String> excludeWords)
+    public async Task<List<string>> ConditionalGet(
+        HashSet<string> allWords, HashSet<string> anyWords, HashSet<string> excludeWords)
     {
         var query = Context.InvertedIndex
             .Where(wd => allWords.Contains(wd.Word) || anyWords.Contains(wd.Word) || excludeWords.Contains(wd.Word))
